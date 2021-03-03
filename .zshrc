d5545b391b354f7657e484dc73f46d12f3b2c653
Venkat Yarlagadda <venkat.yarl@gmail.com>
2021-03-03 13:13:41 -0600

    Added items to installBrew and downloadSoftware

--- .zshrc
+++ .zshrc
@@ -1,13 +1,17 @@
+# Oh My ZSH Settings
 export ZSH="/Users/venkat/.oh-my-zsh"
 ZSH_THEME="agnoster"
 
+# plugins
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
+# Export variables
 export SPARK_HOME=~/projects/AWS_libs/spark-2.4.3-bin-hadoop2.8/
 export SPARK_CONF_DIR=~/projects/AWS_libs/aws-glue-libs/conf
 export PYTHONPATH="${SPARK_HOME}python/:${SPARK_HOME}python/lib/py4j-0.10.7-src.zip:~/projects/AWS_libs/aws-glue-libs/PyGlue.zip:/usr/bin/python3"
+export JAVA_HOME=/usr/local/Cellar/openjdk/15.0.1
 
 ### Alias ###
 alias ll="ls -al"
---
+++ installBrew.sh
@@ -0,0 +1,19 @@
+# All brew installs
+# A
+brew install ack            # https://formulae.brew.sh/formula/ack
+brew install align          # https://formulae.brew.sh/formula/align
+brew install annie          # https://formulae.brew.sh/formula/annie
+brew install awscli         # https://formulae.brew.sh/formula/awscli
+brew install azure-cli      # https://formulae.brew.sh/formula/azure-cli
+brew install bitwarden-cli  # https://formulae.brew.sh/formula/bitwarden-cli
+brew install gradle         # https://formulae.brew.sh/formula/gradle
+brew install groovy         # https://formulae.brew.sh/formula/groovy
+
+# TODO: Look at formulas from L https://formulae.brew.sh/formula/
+
+brew install openjdk        # https://formulae.brew.sh/formula/openjdk
+
+# Download these
+# httpie
+# maven
+# tree
