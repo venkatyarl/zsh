30e810135b290e76e241228c3dae455c2674ed8e
Venkat Yarlagadda <venkat.yarl@gmail.com>
2021-03-03 04:27:54 -0600

    Remove zsh folder after set up is complete.

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,7 @@ echo "------------- Started Clean up -----------"
 rm ~/Desktop/newComputerInstall.sh
 rm ~/Desktop/newComputerSetup/*
 rmdir ~/Desktop/newComputerSetup/
+rm ~/projects/zsh/*
+rmdir ~/projects/zsh/
 
 echo "------------- Finished Clean up -----------"
