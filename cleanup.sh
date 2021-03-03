44377194e4a05abc1d738c2f7d18d0f2f98a45ff
Venkat Yarlagadda <venkat.yarl@gmail.com>
2021-03-03 04:13:26 -0600

    Updated Install and setupGit

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+# Clean up install scripts
+echo "------------- Started Clean up -----------"
+rm ~/Desktop/newComputerInstall.sh
+rm ~/Desktop/newComputerSetup/*
+rmdir ~/Desktop/newComputerSetup/
+
+echo "------------- Finished Clean up -----------"
